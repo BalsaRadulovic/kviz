fc1ad8333684bf86e65e69071758e657b981b2e1
Balsa <bakimne008@gmail.com>
2025-03-30 11:30:34 +0200

    prva verzija

---
+++ index.js
@@ -0,0 +1,20 @@
+let nivoi = document.querySelector('#nivoi')
+
+if (localStorage.getItem('score') !== null) {
+    document.querySelector('#score').innerText = `poeni: ${localStorage.getItem('score')}`;
+} else {
+    localStorage.setItem('score', 0)
+}
+
+for (let i = 1; i <= 30; i++) {
+    let levelState
+    if (i <= parseInt(localStorage.getItem('score'))) {
+        levelState = 'done'
+    } else if (i == parseInt(localStorage.getItem('score'))+1) {
+        levelState = 'notdone'
+    } else {
+        levelState = 'locked'
+    }
+
+    nivoi.innerHTML += `<button class="nivo ${levelState}" onclick="window.location.href='./nivoi/nivo${i}/nivo${i}.html';">${i}</button>`
+}
---
+++ nivoi/nivo3/nivo3.js
@@ -0,0 +1,32 @@
+function checkAnswer() {
+    const answer = document.getElementById('answer-input') ? document.getElementById('answer-input').value.trim().toLowerCase() : null;
+    const resultPopup = document.getElementById('result-popup');
+    const overlay = document.getElementById('overlay');
+    const options = document.querySelectorAll('.option');
+
+    // Disable all options
+    options.forEach(option => option.disabled = true);
+    
+    // Show overlay
+    overlay.style.display = 'block';
+
+    const tacno = (answer === 'merci');
+
+    // Show result in popup
+    if (tacno) {
+        resultPopup.textContent = "Tačno!";
+        resultPopup.style.backgroundColor = 'green';
+    } else {
+        resultPopup.textContent = "Netačno!";
+        resultPopup.style.backgroundColor = 'red';
+    }
+
+    // Show the result popup
+    resultPopup.style.display = 'block';
+
+    // Wait for 3 seconds (3000 milliseconds) and then navigate to the home page
+    setTimeout(() => {
+        if (tacno) localStorage.setItem('score', parseInt(localStorage.getItem('score')) + 1);
+        window.location.href = '../../index.html'; // Redirect to the home page
+    }, 3000); // 3 seconds delay before redirect
+}
