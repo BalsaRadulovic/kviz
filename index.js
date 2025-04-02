3d81ac0debf2ef66f714ed5e408a7bc83782b621
Balsa <bakimne008@gmail.com>
2025-04-03 01:16:51 +0200

    pitanja se rjesavaju samo jedan dan

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let nivoi = document.querySelector('#nivoi')
 
-const currentTime = new Date();
-let dan = currentTime.getHours();
+const startDate = new Date("2025-04-03T00:00:00")
+const currentDate = new Date();
+let dan = 1 + Math.floor((currentDate - startDate) / (1000 * 3600 * 24)) + 22;
 
 if (localStorage.getItem('score') !== null) {
     document.querySelector('#score').innerText = `poeni: ${localStorage.getItem('score')}/${dan}`;
@@ -12,22 +13,26 @@ if (localStorage.getItem('score') !== null) {
     localStorage.setItem('otkljucanoDo', 1);
 }
 
+if (dan > parseInt(localStorage.getItem('otkljucanoDo'))) {
+    localStorage.setItem('otkljucanoDo', dan)
+}
+
 for (let i = 1; i <= 28; i++) {
     if (localStorage.getItem(`pokusaji${i}`) === null) localStorage.setItem(`pokusaji${i}`, 0);
+
+    if (localStorage.getItem(`preskocen${i}`) === null) localStorage.setItem(`preskocen${i}`, i < dan)
+    else localStorage.setItem(`preskocen${i}`, parseInt(localStorage.getItem(`pokusaji${i}`)) === 0)
 
     let levelState, text = ''
     if (i < parseInt(localStorage.getItem('otkljucanoDo'))) {
         levelState = 'done'
-        text = '✅'
+        text = localStorage.getItem(`preskocen${i}`) === 'true' ? '❌' : '✅'
     } else if (i > dan) {
         levelState = 'locked'
         text = '⏳'
     } else if (i == parseInt(localStorage.getItem('otkljucanoDo'))) {
         levelState = 'notdone'
         text = ''
-    } else {
-        levelState = 'locked'
-        text = '❌'
     }
 
     nivoi.innerHTML += `<button class="nivo ${levelState}" onclick="window.location.href='./nivoi/nivo${i}/nivo${i}.html';">${text}</button>`
